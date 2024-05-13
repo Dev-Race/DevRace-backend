6809d717cfef69a98ebb735e0b3b24b96994fb6b
sahyunjin <tkguswls1106@naver.com>
2024-05-13 23:18:29 +0900

    [#1] Setting: 빌드 설정 및 기초 세팅

--- src/main/java/com/sajang/devracebackend/DevraceBackendApplication.java
+++ src/main/java/com/sajang/devracebackend/DevraceBackendApplication.java
@@ -1,10 +1,21 @@
 package com.sajang.devracebackend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+import java.util.TimeZone;
+
+// @EnableJpaAuditing
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})  // 데이터베이스 사용시 해제할것.
 public class DevraceBackendApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevraceBackendApplication.class, args);
