99090a25b742f676dbd8f629b9ac5626748f99f3
sahyunjin <tkguswls1106@naver.com>
2024-05-13 22:45:19 +0900

    Initial commit

---
+++ src/main/java/com/sajang/devracebackend/DevraceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sajang.devracebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevraceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevraceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sajang/devracebackend/DevraceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajang.devracebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevraceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
