3fc2b73b163a505177d090d1eef7877ba649e3b9
JunSang <juns1021@naver.com>
2024-05-16 07:27:00 +0900

    [#23] Feat: User Service 에 로직 작성

--- src/main/java/com/sajang/devracebackend/service/UserService.java
+++ src/main/java/com/sajang/devracebackend/service/UserService.java
@@ -1,4 +1,7 @@
 package com.sajang.devracebackend.service;
 
+import com.sajang.devracebackend.dto.solvedcount.SolvedResponseDto;
+
 public interface UserService {
+    SolvedResponseDto checkUserSolvedCount(String user_id);
 }
--- src/main/java/com/sajang/devracebackend/service/impl/UserServiceImpl.java
+++ src/main/java/com/sajang/devracebackend/service/impl/UserServiceImpl.java
@@ -1,13 +1,31 @@
 package com.sajang.devracebackend.service.impl;
 
+import com.sajang.devracebackend.dto.solvedcount.SolvedResponseDto;
 import com.sajang.devracebackend.repository.UserRepository;
 import com.sajang.devracebackend.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+
+    WebClient webClient = WebClient.builder()
+            .baseUrl("https://solved.ac/api/v3")
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build();
+
+    @Override
+    public SolvedResponseDto checkUserSolvedCount(String user_id){
+        return  webClient.get()
+                .uri("/user/show?handle="+ user_id)
+                .retrieve()
+                .bodyToMono(SolvedResponseDto.class)
+                .block();
+    }
 }
