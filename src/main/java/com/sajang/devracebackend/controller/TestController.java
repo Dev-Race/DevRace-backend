3c8c5b673509db9931c6c8898d35f14f3bdcca75
sahyunjin <tkguswls1106@naver.com>
2024-05-15 23:58:48 +0900

    [#16] Test: CrossOrigin 적용 후 빌드 테스트

--- src/main/java/com/sajang/devracebackend/controller/TestController.java
+++ src/main/java/com/sajang/devracebackend/controller/TestController.java
@@ -1,8 +1,11 @@
 package com.sajang.devracebackend.controller;
 
+import com.sajang.devracebackend.response.ResponseCode;
+import com.sajang.devracebackend.response.ResponseData;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +18,7 @@ public class TestController {
 
     @GetMapping("/health")
     @Operation(summary = "서버 헬스체크 [jwt X]")
-    public String healthCheck() {
-        return "SUCCESS - Health Check";
+    public ResponseEntity<ResponseData> healthCheck() {
+        return ResponseData.toResponseEntity(ResponseCode.HEALTHY_SUCCESS);
     }
 }
--- src/main/java/com/sajang/devracebackend/controller/UserController.java
+++ src/main/java/com/sajang/devracebackend/controller/UserController.java
@@ -3,9 +3,10 @@ package com.sajang.devracebackend.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")  // 차후 SecurityConfig에 대신 만들어줄 예정.
 @Tag(name = "User")
 @RestController
 @RequiredArgsConstructor
