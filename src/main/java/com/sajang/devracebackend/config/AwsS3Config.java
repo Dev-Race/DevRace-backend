4a840a86a86690bc102200695d4ad851c31e1c97
sahyunjin <tkguswls1106@naver.com>
2024-05-16 23:53:20 +0900

    [#28] Fix: Value 어노테이션값 수정

--- src/main/java/com/sajang/devracebackend/config/AwsS3Config.java
+++ src/main/java/com/sajang/devracebackend/config/AwsS3Config.java
@@ -10,12 +10,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class S3Config {
+public class AwsS3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${cloud.aws.credentials.accesskey}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${cloud.aws.credentials.secretkey}")
     private String secretKey;
 
     @Value("${cloud.aws.region.static}")
