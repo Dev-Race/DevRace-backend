6769969038d946c69b46c78a3f5eecd6a7a4ea74
sahyunjin <tkguswls1106@naver.com>
2024-05-18 01:18:23 +0900

    [#32] Feat: 구글 및 깃허브 소셜 OAuth2 기능 구현

--- src/main/java/com/sajang/devracebackend/repository/UserRepository.java
+++ src/main/java/com/sajang/devracebackend/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.sajang.devracebackend.repository;
 
 import com.sajang.devracebackend.domain.User;
+import com.sajang.devracebackend.domain.enums.SocialType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findBySocialTypeAndSocialId(SocialType socialType, String socialId);  // '소셜 타입, 식별자'로 해당 회원을 찾기 위한 메소드 (차후 추가정보 입력 회원가입 가능)
 }
