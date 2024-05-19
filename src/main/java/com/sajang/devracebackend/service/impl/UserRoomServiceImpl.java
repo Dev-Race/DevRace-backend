616631fc4690f74cf22a7ae8b6f2dc7c775cd5d8
sahyunjin <tkguswls1106@naver.com>
2024-05-20 06:03:37 +0900

    [#45] Feat: 여러 사용자 동시 입장 API 구현

--- src/main/java/com/sajang/devracebackend/service/impl/UserRoomServiceImpl.java
+++ src/main/java/com/sajang/devracebackend/service/impl/UserRoomServiceImpl.java
@@ -1,9 +1,12 @@
 package com.sajang.devracebackend.service.impl;
 
+import com.sajang.devracebackend.domain.Chat;
 import com.sajang.devracebackend.domain.Room;
 import com.sajang.devracebackend.domain.User;
+import com.sajang.devracebackend.domain.enums.MessageType;
 import com.sajang.devracebackend.domain.mapping.UserRoom;
 import com.sajang.devracebackend.dto.user.UserEnterRequestDto;
+import com.sajang.devracebackend.repository.ChatRepository;
 import com.sajang.devracebackend.repository.UserRepository;
 import com.sajang.devracebackend.repository.UserRoomRepository;
 import com.sajang.devracebackend.service.RoomService;
@@ -13,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,6 +28,7 @@ public class UserRoomServiceImpl implements UserRoomService {
     private final RoomService roomService;
     private final UserRepository userRepository;
     private final UserRoomRepository userRoomRepository;
+    private final ChatRepository chatRepository;
 
 
     @Transactional
@@ -39,6 +44,7 @@ public class UserRoomServiceImpl implements UserRoomService {
     @Transactional
     @Override
     public void usersEnterRoom(Long roomId, UserEnterRequestDto userEnterRequestDto) {
+        User managerUser = userService.findLoginUser();  // 방장 = 로그인된 사용자
         Room room = roomService.findRoom(roomId);
         List<Long> userIdList = userEnterRequestDto.getUserIdList();
 
@@ -49,8 +55,28 @@ public class UserRoomServiceImpl implements UserRoomService {
                         .room(room)
                         .build())
                 .collect(Collectors.toList());
+        userRooms.add(UserRoom.UserRoomSaveBuilder()
+                        .user(managerUser)
+                        .room(room)
+                        .build());
 
         // 한번에 저장 (여러번 DB에 접근하는것을 방지.)
         userRoomRepository.saveAll(userRooms);
+
+        String message = null;
+        if(userList.size() == 0) message = "'" + managerUser.getNickname() + "'님이 방에 참가하였습니다.";
+        else message = "'" + managerUser.getNickname() + "'님 외 " + userList.size() + "명이 방에 참가하였습니다.";
+
+        Chat chat = Chat.ChatSaveBuilder()
+                .roomId(roomId)
+                .senderId(managerUser.getId())
+                .senderName(managerUser.getNickname())
+                .message(message)
+                .messageType(MessageType.ENTER)
+                .createdTime(LocalDateTime.now())  // 시간을 수동으로 직접 넣어줌. (실시간으로 chat 넘겨주는 시간과 DB에 저장되는 시간을 완전히 같게 하기위해서임.)
+                .build();
+
+        // 입장 채팅 내역 저장
+        chatRepository.save(chat);
     }
 }
