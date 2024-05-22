d8362d0f45cc9fce5b70a101a4490ba2a3ecc81a
sahyunjin <tkguswls1106@naver.com>
2024-05-22 18:31:18 +0900

    [#73] Fix: Docker Network 예외처리 작성

--- .platform/hooks/postdeploy/connect_spring_to_network.sh
+++ .platform/hooks/postdeploy/connect_spring_to_network.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 스크립트 실행중 하나라도 오류 발생 시 자동 종료
+set -e
+
 # Spring 프로젝트 컨테이너 이름 가져오기
 SPRING_CONTAINER_ID=$(docker ps -q -f ancestor=sahyunjin/devrace-image:latest)
 
@@ -9,13 +12,21 @@ if [ -z "$SPRING_CONTAINER_ID" ]; then
   exit 1
 fi
 
-# Spring 프로젝트 컨테이너를 네트워크에 연결
-docker network connect mynetwork $SPRING_CONTAINER_ID
+# Spring 프로젝트 컨테이너가 이미 mynetwork에 연결되어 있는지 확인
+docker network inspect mynetwork --format '{{json .Containers}}' | grep -q "$SPRING_CONTAINER_ID"
+NETWORK_CONNECTED=$?
 
-# 네트워크 연결 성공 여부 확인
-if [ $? -eq 0 ]; then
-  echo "SUCCESS - Spring 컨테이너가 성공적으로 mynetwork에 연결되었습니다."
+# 이미 연결되어 있지 않다면 연결 시도
+if [ "$NETWORK_CONNECTED" -ne 0 ]; then
+  docker network connect mynetwork $SPRING_CONTAINER_ID
+
+  # 네트워크 연결 성공 여부 확인
+  if [ $? -eq 0 ]; then
+    echo "SUCCESS - Spring 컨테이너가 성공적으로 mynetwork에 연결되었습니다."
+  else
+    echo "ERROR - Spring 컨테이너를 네트워크에 연결하는 데 실패했습니다."
+    exit 1
+  fi
 else
-  echo "ERROR - Spring 컨테이너를 네트워크에 연결하는 데 실패했습니다."
-  exit 1
+  echo "Spring 컨테이너가 이미 mynetwork에 연결되어 있습니다."
 fi
