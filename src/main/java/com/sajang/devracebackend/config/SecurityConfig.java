af004fac9e321f1e328ec02a667e9d7e0b928eb6
sahyunjin <tkguswls1106@naver.com>
2024-06-13 14:36:50 +0900

    [#152] Test: permitAll 재적용 테스트

--- src/main/java/com/sajang/devracebackend/config/SecurityConfig.java
+++ src/main/java/com/sajang/devracebackend/config/SecurityConfig.java
@@ -58,9 +58,9 @@ public class SecurityConfig {
                             .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                             .requestMatchers(HttpMethod.POST, "/signup").hasAuthority("ROLE_GUEST")  // '/signup' api는 ROLE_GUEST 권한 로그인 사용자만 사용 가능. 이는 DB 속성값 & 헤더의 jwt 토큰에 등록해둔 권한도 바꾸어 재발급 받아야 한다.
 
-//                            .requestMatchers("/", "/error", "/favicon.ico", "/v3/api-docs/**", "/swagger-ui/**", "/swagger/**", "/health").permitAll()
-//                            .requestMatchers("/ws/**", "/oauth2/**", "/reissue").permitAll()
-                             .requestMatchers("/**").permitAll()  // Test 용도
+                            .requestMatchers("/", "/error", "/favicon.ico", "/v3/api-docs/**", "/swagger-ui/**", "/swagger/**", "/health").permitAll()
+                            .requestMatchers("/ws/**", "/oauth2/**", "/reissue").permitAll()
+                            // .requestMatchers("/**").permitAll()  // Test 용도
 
                             .anyRequest().hasAnyAuthority("ROLE_USER", "ROLE_ADMIN");  // permit 지정한 경로들 외에는 전부 USER나 ADMIN 권한이 있어야지 url을 이용 가능하다. (GUEST 불가능)
                 })
